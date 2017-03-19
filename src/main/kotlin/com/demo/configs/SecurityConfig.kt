a10338b7649b72304d76f10073cf2a6972b5fbc3
Alexey <leron2k@yandex.ru>
2017-03-19 14:12:39 +0300

    oauth2 (why??)

--- src/main/kotlin/com/demo/configs/SecurityConfig.kt
+++ src/main/kotlin/com/demo/configs/SecurityConfig.kt
@@ -3,6 +3,7 @@ package com.demo.configs
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -10,19 +11,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true) //prePostEnabled = true
-//open class SecurityConfig @Autowired constructor(userDetailsService: UserDetailsService) : WebSecurityConfigurerAdapter() {
-//
-//    override fun configure(http: HttpSecurity?) {
-//        http
-//                ?.headers()?.frameOptions()?.sameOrigin()
-//                ?.and()
-//                ?.addFilterAfter(null, UsernamePasswordAuthenticationFilter::class.java)
-//                ?.authorizeRequests()
-//                ?.antMatchers()
-//    }
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //securedEnable = true
+open class SecurityConfig @Autowired constructor(userDetailsService: UserDetailsService) : WebSecurityConfigurerAdapter() {
+
+
+    override fun configure(http: HttpSecurity?) {
+        http!!
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .addFilterAfter(null, UsernamePasswordAuthenticationFilter::class.java)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and().csrf().disable();
+    }
 //
 //    @Bean(name = "restTokenAuthenticationFilter")
 //    fun restTokenAuthenticationFilter(): RestTokenAuthenticationFilter {
@@ -31,4 +36,4 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 //        restTokenAuthenticationFilter.setAuthenticationManager(tokenAuthenticationManager)
 //        return restTokenAuthenticationFilter
 //    }
-//}
+}
