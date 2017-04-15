fc9d8ec97d2e9503b7c410ef3ca3c1119e90d1fa
Alexey <leron2k@yandex.ru>
2017-04-15 14:02:55 +0300

    subscription controller

--- src/main/kotlin/com/demo/controllers/SubscriptionController.kt
+++ src/main/kotlin/com/demo/controllers/SubscriptionController.kt
@@ -1,6 +1,7 @@
 package com.demo.controllers
 
 import com.demo.dto.SubscriptionDto
+import com.demo.exceptions.NotFoundException
 import com.demo.services.SubscriptionService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
@@ -21,6 +22,23 @@ class SubscriptionController
     fun update(@PathVariable id: Long, @RequestBody(required = true) dto: SubscriptionDto): ResponseEntity<SubscriptionDto> {
         val updatedSubscription = subscriptionService.update(id, dto)
         return ResponseEntity.ok(SubscriptionDto(updatedSubscription))
+    }
+
+    @RequestMapping(value = "",
+            method = arrayOf(RequestMethod.POST),
+            consumes = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE),
+            produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
+    fun create(@RequestBody(required = true) dto: SubscriptionDto): ResponseEntity<SubscriptionDto> {
+        val newSubscription = subscriptionService.create(dto)
+        return ResponseEntity.ok(SubscriptionDto(newSubscription))
+    }
+
+    @RequestMapping(value = "{id}/",
+            method = arrayOf(RequestMethod.GET),
+            produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
+    fun read(@PathVariable id: Long): ResponseEntity<SubscriptionDto> {
+        val subscription = subscriptionService.read(id)
+        return ResponseEntity.ok(SubscriptionDto(subscription.orElseThrow(NotFoundException.sup)))
     }
 
 }
