abd64c8af3fb6ba6ba13eb1badd1e40f6540f80e
Alexey <leron2k@yandex.ru>
2017-05-16 19:17:34 +0300

    version up - ??

--- src/main/kotlin/com/demo/PlantApplication.kt
+++ src/main/kotlin/com/demo/PlantApplication.kt
@@ -1,11 +1,9 @@
 package com.demo
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-@EnableAutoConfiguration
 open class PlantApplication
 
 fun main(args: Array<String>) {
